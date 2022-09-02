f981afec271c2074f5cca39955983cbd2f4e65f4
Shrey Pandey <shreypandey27@gmail.com>
2022-09-02 09:44:00 +0000

    api updated

--- index.js
+++ index.js
@@ -3,15 +3,33 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 
+/*
+var config = {
+  method: 'get',
+  
+url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=new&key=AIzaSyCtdaHsIicmzziPVLzBFe8RVKvXdZGHHFQ',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data.predictions.map(item => item.description)));
+})
+.catch(function (error) {
+  console.log(error);
+});
+*/
+
 app.use(bodyParser.json());
 app.use(cors());
 
 const productRouter = require('./router.js');
 
-function serverTimeLog(req, res, next) {
-  console.log('Time', Date.now())
-  next();
-}
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.use('/hotels', productRouter);
 
 function routeNotFound(req, res, next) {
   res.status(404).json({ message: 'Route not found' })
@@ -21,16 +39,9 @@ function errorHandler(err, req, res, next){
   res.status(500).json({message:err.message, success:false})
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
-});
+app.use(routeNotFound)
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('server started');
 });
-app.use(serverTimeLog)
-app.use('/hotels', productRouter);
-
-app.use(routeNotFound)
-app.use(errorHandler);
-
--- router.js
+++ router.js
@@ -1,9 +1,19 @@
 const hotels = require('./database.js');
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const mySecret = process.env['GOOGLE_KEY']
 
-router.route("/").get((_, res) => {
-    res.json(hotels)
-})
+router.route("/")
+  .post(async (req, res) => {
+    const { input } = req.body;
+    let URL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${mySecret}`
+    let response = await axios.get(URL)
+    let dataSet = response.data.predictions.map(item => item.description)
+    let search = input.toLowerCase();
+    let hotelSet = hotels.filter(hotel => hotel.title.toLowerCase().includes(search) || hotel.address.toLowerCase().includes(search));
+    let result = { locations: dataSet, hotels: hotelSet }
+    res.json(result);
+  })
 
 module.exports = router;
